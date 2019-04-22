ea6ab3d757201892786d140b947d7372cb01ba1e
Gustavo Murayama <gmurayama@outlook.com>
2019-04-21 21:24:08 -0300

    Comentários com explicação/documentação do código

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ struct RisosFS {
 }
 
 impl RisosFS {
+    /// Inicializa o FS com o tamanho especificado em `memory_size` com blocos de memória de tamanho
+    /// `block_size`.
     fn new() -> RisosFS {
         let memory_size: usize = 1024 * 1024 * 10;
         let block_size: usize = 1024;
@@ -24,6 +26,7 @@ impl RisosFS {
     }
 }
 
+/// Implementação das funções disponíveis na lib `rust-fuse`
 impl Filesystem for RisosFS {
     fn lookup(
         &mut self, 
@@ -34,14 +37,17 @@ impl Filesystem for RisosFS {
     ) {
         let file_name = name.to_str().unwrap();
 
+        // Procura pelo `path` do arquivo na tabela de inode
         let hashmap_item = self.disk.get_inode_table().iter()
             .find(|(_, inode)| inode.path == file_name);
 
         match hashmap_item {
+            // Se houver um item com o `path`, então ele dá um `reply` com os atributos do arquivo
             Some ((_, inode)) => {
                 let ttl = Timespec::new(1, 0);
                 reply.entry(&ttl, &inode.attributes, 0);
             }
+            // Caso não seja encontrado, retorna um código de erro
             None => reply.error(ENOSYS)
         }
     }
