d91a9b6c0fb3249cfd9ba13a3d78c87c45d62991
lucastornai <lucas.tornai00@gmail.com>
2019-04-28 00:25:45 -0300

    Now create files to hold filesystem and can properly read them

---
+++ src/serialization.rs
@@ -0,0 +1,46 @@
+use serde::{Serialize, Deserialize};
+use fuse::{FileAttr, FileType};
+use time::Timespec;
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "Timespec")]
+pub struct TimespecDef {
+    pub sec: i64,
+    pub nsec: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "FileType")]
+pub enum FileTypeDef {
+    NamedPipe,
+    CharDevice,
+    BlockDevice,
+    Directory,
+    RegularFile,
+    Symlink,
+    Socket,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "FileAttr")]
+pub struct FileAttrDef {
+    pub ino: u64,
+    pub size: u64,
+    pub blocks: u64,
+    #[serde(with = "TimespecDef")]
+    pub atime: Timespec,
+    #[serde(with = "TimespecDef")]
+    pub mtime: Timespec,
+    #[serde(with = "TimespecDef")]
+    pub ctime: Timespec,
+    #[serde(with = "TimespecDef")]
+    pub crtime: Timespec,
+    #[serde(with = "FileTypeDef")]
+    pub kind: FileType,
+    pub perm: u16,
+    pub nlink: u32,
+    pub uid: u32,
+    pub gid: u32,
+    pub rdev: u32,
+    pub flags: u32,
+}
